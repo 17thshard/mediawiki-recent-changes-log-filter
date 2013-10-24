3182011e44eeab96a59c37c749a89f17d2da56db
Patrick Westerhoff <PatrickWesterhoff@gmail.com>
2013-10-25 01:07:56 +0200

    Add extension files

---
+++ RecentChangesLogFilter.i18n.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * RecentChangesLogFilter extension, internationalization
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Patrick Westerhoff
+ */
+$messages['en'] = array(
+	'recentchangeslogfilter-desc' => 'This extension filters log entries from [[Special:RecentChanges]]',
+	'recentchangeslogfilter-hidelogs' => '$1 all logs'
+);
+
+/** German (Deutsch)
+ * @author Patrick Westerhoff
+ */
+$messages['de'] = array(
+	'recentchangeslogfilter-desc' => 'Diese Erweiterung filtert Logbuch-Einträge auf [[Spezial:Letzte_Änderungen]]',
+	'recentchangeslogfilter-hidelogs' => 'Alle Logbücher $1'
+);
+
+/** Message documentation
+ * @author Patrick Westerhoff
+ */
+$messages['qqq'] = array(
+		'recentchangeslogfilter-desc' => "{{desc}}",
+		'recentchangeslogfilter-hidelogs' => 'Recent changes filter message with $1 being the link'
+);
---
+++ RecentChangesLogFilter.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * RecentChangesLogFilter extension
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Patrick Westerhoff <PatrickWesterhoff@gmail.com>
+ */
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die();
+}
+
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'RecentChangesLogFilter',
+	'author' => 'Patrick Westerhoff',
+	'descriptionmsg' => 'recentchangeslogfilter-desc',
+	'version'  => 1.0,
+);
+
+/* Extension setup */
+$dir = dirname( __FILE__ );
+$wgExtensionMessagesFiles['RecentChangesLogFilter'] = $dir . '/RecentChangesLogFilter.i18n.php';
+
+
+/**
+ * Array of log types to filter by default. Default is `newusers`.
+ */
+$wgRecentChangesLogFilterTypes = array( 'newusers' );
+
+/**
+ * Add the `hidelogs` filter to the recent changes filter list.
+ *
+ * @return true
+ */
+$wgHooks['SpecialRecentChangesFilters'][] = function( $special, &$filters ) {
+	$filters['hidelogs'] = array(
+		'msg' => 'recentchangeslogfilter-hidelogs',
+		'default' => true
+	);
+
+	return true;
+};
+
+/**
+ * Modify the recent changes query to hide log entries from the list. This only
+ * occurs when the `hidelogs` filter is enabled (which is the default).
+ *
+ * @return true
+ */
+$wgHooks['SpecialRecentChangesQuery'][] = function( &$conds, &$tables, &$join_conds, $opts, &$query_options, &$fields ) {
+	$dbr = wfGetDB( DB_SLAVE );
+
+	if ($opts['hidelogs']) {
+		$conds[] = '( rc_log_type IS NULL OR rc_log_type != ' . $dbr->addQuotes( 'newusers' ) . ' )';
+	}
+
+	return true;
+};
